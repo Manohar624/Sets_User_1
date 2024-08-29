9d4412993597d51a5bdfcc0c19380669e7715143
Manohar624 <manoharnaidu4703@gmail.com>
2024-08-29 17:47:49 +0530

    Manohar

--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,15 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 public class LinkedHashSets{
 public static void main(String[]args){
-  // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+     LinkedHashSet<Person> p = new LinkedHashSet<>();
+  p.add(new Person(1, "Manohar", 4000));
+  p.add(new Person(2, "Hari", 5000));
+  p.add(new Person(3, "Bhuvi", 4000));
+
+  Iterator<Person> i = p.iterator();
+  while (i.hasNext()) {
+      System.out.println(i.next());
+  }
+}
 }
--- Person.java
+++ Person.java
@@ -1,3 +1,28 @@
 public class Person{
-// prepare a Person Data With Getters and Setters,Constructor,ToString and Override HashCode & Equals Methods in it
-}
+    private int id;
+    private String name;
+    private int salary;
+    public Person(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+  
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+ 
+    @Override
+    public String toString() {
+        return "Person{id=" + id + ", name='" + name + "', salary=" + salary + "}";
+    }
+  }
+
